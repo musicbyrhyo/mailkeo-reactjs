9ab89d14ec46d12bb1f463b0cfc40e92c033f0f4
sahilpabale <dev.sahilpabale@gmail.com>
2021-07-02 12:26:28 +0530

    fuking last

--- src/pages/Audiences.js
+++ src/pages/Audiences.js
@@ -1,169 +1,157 @@
-import axios from 'axios'
-import { useEffect, useState } from 'react'
-import styled from 'styled-components'
-import { ChannelB } from '../components/Channels'
-import { CreateAudience } from '../components/CreateAudience'
-import { DashBoardNav } from '../components/DashBoardNav'
-import { Colors } from '../components/StyledComponents'
+import axios from "axios";
+import { useEffect, useState } from "react";
+import styled from "styled-components";
+import { ChannelB } from "../components/Channels";
+import { CreateAudience } from "../components/CreateAudience";
+import { DashBoardNav } from "../components/DashBoardNav";
+import { Colors } from "../components/StyledComponents";
 
-const hostname = process.env.REACT_APP_API
-const token = localStorage.getItem('token')
+const hostname = process.env.REACT_APP_API;
+const token = localStorage.getItem("token");
 
 export const Audiences = () => {
+  const [Audiences, setAudiences] = useState([
+    {
+      a_subscribers: [],
+      a_created: "",
+    },
+  ]);
 
-    const [Audiences, setAudiences] = useState([{
-        a_subscribers:[],
-        a_created: ''
-    }])
+  const [CreateAudienceTrigger, setCreateAudienceTrigger] = useState(false);
 
-    const [CreateAudienceTrigger, setCreateAudienceTrigger] = useState(false)
+  const ViewCreateAudience = () => {
+    setCreateAudienceTrigger(true);
+  };
 
-    const ViewCreateAudience = () => {
-        setCreateAudienceTrigger(true);
-    }
+  const Active = {
+    one: {
+      opacity: "1",
+    },
+    two: {
+      opacity: "1",
+    },
+    three: {
+      opacity: "0.5",
+    },
+    four: {
+      opacity: "1",
+    },
+    left: "5000px",
+  };
 
-    const Active = {
-        one: {
-            opacity: '1',
-        },
-        two: {
-            opacity: '1',
-        },
-        three: {
-            opacity: '0.5',
-        },
-        four: {
-            opacity: '1',
-        },
-        left: '5000px'
-    }
-
-    const getAudiences = async () => {
-
-        try {
-            
-            const response = await axios.post(`${hostname}/audience/all`,{},{
-                headers:{
-                    'authorization': token
-                }
-            })
-    
-            console.log(response);
-            setAudiences(response.data)
-
-        } catch (error) {
-            
-            console.log(error);
-
+  const getAudiences = async () => {
+    try {
+      const response = await axios.post(
+        `${hostname}/audience/all`,
+        {},
+        {
+          headers: {
+            authorization: token,
+          },
         }
+      );
 
+      console.log(response);
+      setAudiences(response.data);
+    } catch (error) {
+      console.log(error);
     }
+  };
 
-    const AudienceList = Audiences.map((audience) => <ChannelB title={audience.a_name} subscribers={audience.a_subscribers.length} id={audience._id} date={audience.a_created.slice(0,10)} />)
+  const AudienceList = Audiences.map((audience) => (
+    <ChannelB
+      title={audience.a_name}
+      subscribers={audience.a_subscribers.length}
+      id={audience._id}
+      date={audience.a_created.slice(0, 10)}
+    />
+  ));
 
-    useEffect(() => {
-        
-        getAudiences()
+  useEffect(() => {
+    getAudiences();
+  }, []);
 
-    }, [])
-
-    return (
-        <>
-            <DashBoardNav Active={Active}/>
-            <CampaignHolder>
-                <HeaderHolder>
-                    <Header>
-                        Your Audiences
-                    </Header>
-                    <CreateCampaign onClick={ViewCreateAudience} >
-                        Create New Audience
-                    </CreateCampaign>
-                </HeaderHolder>
-                <div>
-                    <SubHeader>
-                        All Audiences
-                    </SubHeader>
-                    <Divider></Divider>
-                    <div>
-                        {AudienceList}
-                    </div>
-                </div>
-            </CampaignHolder>
-            <CreateAudience Trigger={CreateAudienceTrigger} setTrigger={setCreateAudienceTrigger} />
-        </>
-    )
-}
+  return (
+    <>
+      <DashBoardNav Active={Active} />
+      <CampaignHolder>
+        <HeaderHolder>
+          <Header>Your Audiences</Header>
+          <CreateCampaign onClick={ViewCreateAudience}>
+            Create New Audience
+          </CreateCampaign>
+        </HeaderHolder>
+        <div>
+          <SubHeader>All Audiences</SubHeader>
+          <Divider></Divider>
+          <div>{AudienceList}</div>
+        </div>
+      </CampaignHolder>
+      <CreateAudience
+        Trigger={CreateAudienceTrigger}
+        setTrigger={setCreateAudienceTrigger}
+      />
+    </>
+  );
+};
 
 const Divider = styled.div`
-
-    height: 1px;
-    width: 100%;
-    background-color: ${Colors.profile};
-    margin-top: 8px;
-
-`
+  height: 1px;
+  width: 100%;
+  background-color: ${Colors.profile};
+  margin-top: 8px;
+`;
 
 const SubHeader = styled.div`
-
-    font-size: 16px;
-    color: ${Colors.profile};
-`
+  font-size: 16px;
+  color: ${Colors.profile};
+`;
 
 const Header = styled.div`
-
-    font-size: 32px;
-    font-weight: 800;
-
-`
+  font-size: 32px;
+  font-weight: 800;
+`;
 
 const HeaderHolder = styled.div`
-
-    margin: 40px 0px;
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-    width: 100%;
-
-`
+  margin: 40px 0px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  width: 100%;
+`;
 
 const CreateCampaign = styled.a`
+  font-size: 12px;
+  letter-spacing: 0.5px;
+  font-weight: 400;
+  border-radius: 5px;
+  padding: 10px;
+  height: 40px;
+  width: 200px;
+  background-color: ${Colors.primary};
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  cursor: pointer;
+  transition: 150ms ease-in-out;
 
-    font-size: 12px;
-    letter-spacing: 0.5px;
-    font-weight: 400;
-    border-radius: 5px;
-    padding: 10px;
-    height: 40px;
-    width: 200px;
-    background-color: ${Colors.primary};
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    cursor: pointer;
-    transition: 150ms ease-in-out;
-
-    &:hover{
-        box-shadow: 0 5px 5px rgba(0, 0, 0, 0.397);
-        transition: 100ms ease-in-out;
-    }
-
-`
+  &:hover {
+    box-shadow: 0 5px 5px rgba(0, 0, 0, 0.397);
+    transition: 100ms ease-in-out;
+  }
+`;
 
 const CampaignHolder = styled.div`
+  padding-left: 20%;
+  padding-right: 20%;
 
-    padding-left: 20%;
-    padding-right: 20%;
+  @media (max-width: 1366px) {
+    padding-left: 5%;
+    padding-right: 5%;
+  }
 
-    @media (max-width: 1366px) 
-        {
-            padding-left: 5%;
-            padding-right: 5%;
-        }
-
-    @media (max-width: 800px) 
-        {
-            padding-left: 20px;
-            padding-right: 20px;
-        }
-
-`
+  @media (max-width: 800px) {
+    padding-left: 20px;
+    padding-right: 20px;
+  }
+`;
--- src/pages/Overview.js
+++ src/pages/Overview.js
@@ -10,146 +10,147 @@ const token = localStorage.getItem("token");
 const username = localStorage.getItem("username");
 
 export const Overview = () => {
-    const Active = {
-        one: {
-            opacity: "0.5",
-        },
-        two: {
-            opacity: "1",
-        },
-        three: {
-            opacity: "1",
-        },
-        four: {
-            opacity: "1",
-        },
-        left: "5000px",
-    };
+  const Active = {
+    one: {
+      opacity: "0.5",
+    },
+    two: {
+      opacity: "1",
+    },
+    three: {
+      opacity: "1",
+    },
+    four: {
+      opacity: "1",
+    },
+    left: "5000px",
+  };
 
-    const [Campaigns, setCampaigns] = useState([]);
-    const [Audiences, setAudiences] = useState([
+  const [Campaigns, setCampaigns] = useState([]);
+  const [Audiences, setAudiences] = useState([
+    {
+      a_subscribers: [],
+      a_created: "",
+    },
+  ]);
+
+  const getCampaign = async () => {
+    try {
+      const response = await axios.post(
+        `${hostname}/campaign/getall`,
+        {},
         {
-            a_subscribers: [],
-            a_created: "",
-        },
-    ]);
-
-    const getCampaign = async () => {
-        try {
-            const response = await axios.post(
-                `${hostname}/campaign/getall`,
-                {},
-                {
-                    headers: {
-                        authorization: token,
-                    },
-                }
-            );
-
-            console.log(response);
-            setCampaigns(response.data);
-        } catch (error) {
-            console.log(error);
+          headers: {
+            authorization: token,
+          },
         }
-    };
+      );
 
-    const getAudiences = async () => {
-        try {
-            const response = await axios.post(
-                `${hostname}/audience/all`,
-                {},
-                {
-                    headers: {
-                        authorization: token,
-                    },
-                }
-            );
+      console.log(response);
+      setCampaigns(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
-            console.log(response);
-            setAudiences(response.data);
-        } catch (error) {
-            console.log(error);
+  const getAudiences = async () => {
+    try {
+      const response = await axios.post(
+        `${hostname}/audience/all`,
+        {},
+        {
+          headers: {
+            authorization: token,
+          },
         }
-    };
+      );
 
-    const checkUser = async () => {
-        if (!token) return (window.location.href = "/login");
-    };
+      console.log(response);
+      setAudiences(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
-    const CampaignList = Campaigns.map((campaign) => (
-        <ChannelA
-            title={campaign.c_name}
-            date={campaign.c_date.slice(0, 10)}
-            id={campaign._id}
-            complete={campaign.c_complete}
-        />
-    ));
+  const checkUser = async () => {
+    if (!token) return (window.location.href = "/login");
+  };
 
-    const AudienceList = Audiences.map((audience) => (
-        <ChannelB
-            title={audience.a_name}
-            subscribers={audience.a_subscribers.length}
-            id={audience._id}
-            date={audience.a_created.slice(0, 10)}
-        />
-    ));
+  const CampaignList = Campaigns.map((campaign) => (
+    <ChannelA
+      title={campaign.c_name}
+      date={campaign.c_date.slice(0, 10)}
+      id={campaign._id}
+      complete={campaign.c_complete}
+    />
+  ));
 
-    useEffect(() => {
-        getCampaign();
-        getAudiences();
-        checkUser();
-    }, []);
+  const AudienceList = Audiences.map((audience) => (
+    <ChannelB
+      title={audience.a_name}
+      subscribers={audience.a_subscribers.length}
+      id={audience._id}
+      date={audience.a_created.slice(0, 10)}
+    />
+  ));
 
-    return (
-        <>
-            <DashBoardNav Active={Active} />
-            <DashBoardHolder>
-                <WelcomeText>Welcome Back, {username} </WelcomeText>
-                <SectionB>
-                    <SectionHeader>Campaigns</SectionHeader>
-                    <div>{CampaignList}</div>
-                </SectionB>
-                <SectionC>
-                    <SectionHeader>Audiences</SectionHeader>
-                    <div>{AudienceList}</div>
-                </SectionC>
-            </DashBoardHolder>
-        </>
-    );
+  useEffect(() => {
+    getCampaign();
+    getAudiences();
+    checkUser();
+  }, []);
+
+  return (
+    <>
+      <DashBoardNav Active={Active} />
+      <DashBoardHolder>
+        <WelcomeText>Welcome Back, {username} </WelcomeText>
+        <SectionB>
+          <SectionHeader>Campaigns</SectionHeader>
+          <div>{CampaignList}</div>
+        </SectionB>
+        <SectionC>
+          <SectionHeader>Audiences</SectionHeader>
+          <div>{AudienceList}</div>
+        </SectionC>
+      </DashBoardHolder>
+    </>
+  );
 };
 
 const SectionHeader = styled.div`
-    font-size: 20px;
-    border-bottom: 1px solid ${Colors.dark};
-    padding-bottom: 12px;
+  font-size: 20px;
+  border-bottom: 1px solid ${Colors.dark};
+  padding-bottom: 12px;
 `;
 
 const SectionC = styled.div`
-    margin-bottom: 60px;
+  margin-bottom: 60px;
 `;
 
 const SectionB = styled.div`
-    margin-bottom: 60px;
+  margin-bottom: 60px;
 `;
 
 const DashBoardHolder = styled.div`
-    padding-left: 20%;
-    padding-right: 20%;
+  padding-left: 20%;
+  padding-right: 20%;
+  overflow-x: hidden;
 
-    @media (max-width: 1366px) {
-        padding-left: 5%;
-        padding-right: 5%;
-    }
+  @media (max-width: 1366px) {
+    padding-left: 5%;
+    padding-right: 5%;
+  }
 
-    @media (max-width: 800px) {
-        padding-left: 20px;
-        padding-right: 20px;
-    }
+  @media (max-width: 800px) {
+    padding-left: 20px;
+    padding-right: 20px;
+  }
 `;
 
 const WelcomeText = styled.div`
-    font-size: 32px;
-    font-weight: 800;
-    color: ${Colors.dark};
-    margin: 48px 0px;
+  font-size: 32px;
+  font-weight: 800;
+  color: ${Colors.dark};
+  margin: 48px 0px;
 `;
